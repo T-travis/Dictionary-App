d920242c542d5eb6b5fec90de4e2e90bc98b9969
T-travis <ttravis1111@gmail.com>
2019-04-03 14:50:38 -0700

    updated js and css, added favicon

---
+++ www/js/getWordDefinition.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// get word definition from user input
+async function getWordDef(input) {
+  try {
+    input = input.toLowerCase(); // make lowercase for matching in db
+    const results = await axios.get(`http://localhost:3000/api.domain.com/v1/words/${input}`);
+    return results;
+  } catch (error) {
+    return error;
+  }
+}
+
+// validate input: not empty and less than 40 characters
+function validate(input) {
+  if (!input || input.length > 40) {
+    alert('Before pressing the Search button, enter a word of less than 40 charaters');
+    return false;
+  }
+  return true;
+}
+
+// handle http error messages
+function handleErrorMessage(status) {
+  $('#errorSection').show();
+  let backgroundColor = document.getElementById('errorSection');
+  backgroundColor.className = 'red'; // error message background color is red
+  if (status === 400) {
+    $("#errorCode").text(status);
+    $("#errorMessage").text("Bad Request Error");
+  } else if (status === 404) {
+    $("#errorCode").text("Word Not Found");
+    $("#errorMessage").text("Try another word");
+    backgroundColor.className = '';
+    backgroundColor.className = 'yellow'; // only this one is yellow because it is just not found word
+  } else if (status === 500) {
+    $("#errorCode").text(status);
+    $("#errorMessage").text("Internal Server Error");
+  } else {
+    // handle unkown error response
+    $("#errorCode").text(status);
+    $("#errorMessage").text("OOPS, Unkown Error");
+  }
+}
+
+// handle click events of the search button
+$('.search').click(async function () {
+  // hide error section
+  $('#errorSection').hide();
+  // clear search word
+  $('#word').text("");
+  // clear definition
+  $('#definition').text("");
+  // get input from user
+  let input = $("#autocomplete-input").val();
+  $("#autocomplete-input").val("");
+  // validate input and handle results
+  if (validate(input)) {
+    const results = await getWordDef(input);
+    // if response then there is an error
+    if (results.response) {
+      // set html tags for error
+      //console.log(results.response.status);
+      const status = results.response.status;
+      handleErrorMessage(status);
+    } else if (results.data) {
+      // set html tags for success
+      //console.log(results.data);
+      // add word to span
+      $('#word').text(results.data.word);
+      // add definition to span
+      $('#definition').text(results.data.definition);
+    } else {
+      // 500 can be found above in results.response but if the db goes down
+      // this will be triggered 
+      handleErrorMessage(500);
+    }
+  }
+});
---
+++ www/js/getWordMatches.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// as characters are entered in the the input tag, get 
+// relevant matches from db
+async function getCharMatches(chars) {
+  try {
+    chars = chars.toLowerCase();
+    const results = await axios.get(`http://localhost:3000/api.domain.com/v1/words/input-chars/${chars}`);
+    return results;
+  } catch (error) {
+    console.log("error");
+    return error;
+  }
+}
+
+// when documnet object is ready
+$(document).ready(function () {
+
+  // hide error section 
+  $('#errorSection').hide();
+
+  // jquery version
+  $('#autocomplete-input').on('input', async function (e) {
+
+    //console.log($(this).val()) // get the current value of the input field.
+
+    // get matches for current char entered into input
+    const results = await getCharMatches(this.value);
+    //console.log(results.data);
+    const hints = {};
+    // iterate over results and store in hints
+    for (let object of results.data) {
+      hints[object.word] = null; // key needs to be null
+    }
+
+    // this makes the autocomplete dropdown
+    $('#autocomplete-input').autocomplete({
+      data: hints,
+      limit: 4,
+      minLength: 1,
+      autoFocus: true
+    });
+
+  });
+
+});
